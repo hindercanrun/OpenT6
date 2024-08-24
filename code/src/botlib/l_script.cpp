0723665fbed9b6d42dcf0f4bf30a15a5827c5ca8
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2024-08-24 17:40:30 +0800

    l_script.cpp work

--- code/src/botlib/l_script.cpp
+++ code/src/botlib/l_script.cpp
@@ -27,7 +27,16 @@ ScriptWarning
 */
 void ScriptWarning(script_s *script, const char *str, ...)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	char text[1024];
+	va_list ap;
+
+	va_start(ap, str);
+
+	if ((script->flags & 2) == 0)
+	{
+		_vsnprintf(text, 0x400u, str, ap);
+		Com_PrintWarning(21, "Warning: file %s, line %d: %s\n", script->filename, script->line, text);
+	}
 }
 
 /*
@@ -37,21 +46,24 @@ PS_ReadWhiteSpace
 */
 int PS_ReadWhiteSpace(script_s *script)
 {
+	// straight from ida
+	// clean up soon
+
 	char *script_p;
 
 LABEL_1:
-	if ( *script->script_p <= 32 )
+	if (*script->script_p <= 32)
 	{
 		do
 		{
 			script_p = script->script_p;
 
-			if ( !*script_p )
+			if (!*script_p)
 			{
 				return 0;
 			}
 
-			if ( *script_p == 10 )
+			if (*script_p == 10)
 			{
 				++script->line;
 			}
@@ -59,30 +71,30 @@ LABEL_1:
 			script->script_p = script_p + 1;
 		}
 
-		while ( script_p[1] <= 32 );
+		while (script_p[1] <= 32);
 	}
 
 	char *v2 = script->script_p;
 
-	if ( *v2 == 47 )
+	if (*v2 == 47)
 	{
 		char v3 = v2[1];
 		char *v4 = v2 + 1;
 
-		if ( v3 == 47 )
+		if (v3 == 47)
 		{
 			script->script_p = v4;
 
-			while ( 1 )
+			while (1)
 			{
 				script->script_p = ++v4;
 
-				if ( !*v4 )
+				if (!*v4)
 				{
 					return 0;
 				}
 
-				if ( *v4 == 10 )
+				if (*v4 == 10)
 				{
 					++script->line;
 
@@ -91,29 +103,29 @@ LABEL_1:
 			}
 		}
 
-		if ( v3 == 42 )
+		if (v3 == 42)
 		{
 			script->script_p = v4;
 
-			while ( 1 )
+			while (1)
 			{
 				script->script_p = ++v4;
 
-				if ( !*v4 )
+				if (!*v4)
 				{
 					return 0;
 				}
 
-				if ( *v4 == 10 )
+				if (*v4 == 10)
 				{
 					++script->line;
 				}
 
-				if ( *v4 == 42 && v4[1] == 47 )
+				if (*v4 == 42 && v4[1] == 47)
 				{
 					v4 = ++script->script_p;
 
-					if ( !*v4 )
+					if (!*v4)
 					{
 						return 0;
 					}
@@ -121,7 +133,7 @@ LABEL_20:
 					char *v5 = v4 + 1;
 					script->script_p = v5;
 
-					if ( *v5 )
+					if (*v5)
 					{
 						goto LABEL_1;
 					}
@@ -168,20 +180,28 @@ int PS_ReadName(script_s *script, token_s *token)
 	token->type = 4;
 	int result = 1;
 
-	while ( 1 )
+	while (1)
 	{
 		token->string[v2] = *script->script_p++;
-		if ( ++v2 >= 1024 )
+
+		if (++v2 >= 1024)
+		{
 			break;
+		}
+
 		char v4 = *script->script_p;
-		if ( (v4 < 97 || v4 > 122) && (v4 < 65 || v4 > 90) && (v4 < 48 || v4 > 57) && v4 != 95 )
+
+		if ((v4 < 97 || v4 > 122) && (v4 < 65 || v4 > 90) && (v4 < 48 || v4 > 57) && v4 != 95)
 		{
 			token->string[v2] = 0;
 			token->subtype = v2;
+
 			return result;
 		}
 	}
+
 	ScriptError(script, "name longer than MAX_TOKEN = %d", 1024);
+
 	return 0;
 }
 
@@ -227,16 +247,16 @@ int PS_ReadPrimitive(script_s *script, token_s *token)
 	int result;
 	int v2 = 0;
 
-	for ( result = 1; *script->script_p > 32; ++v2 )
+	for (result = 1; *script->script_p > 32; ++v2)
 	{
 		char v4 = *script->script_p;
 
-		if ( v4 == 59 )
+		if (v4 == 59)
 		{
 			break;
 		}
 
-		if ( v2 >= 1024 )
+		if (v2 >= 1024)
 		{
 			ScriptError(script, "primitive token longer than MAX_TOKEN = %d", 1024);
 			return 0;
@@ -271,7 +291,24 @@ StripDoubleQuotes
 */
 void StripDoubleQuotes(char *string)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (*string == 34)
+	{
+		// needs a clean up
+
+		char *v1 = string + 1;
+		do
+		{
+			char v2 = *v1;
+			*(v1 - 1) = *v1;
+			++v1;
+		}
+		while (v2);
+	}
+
+	if (string[strlen(string) - 1] == 34)
+	{
+		string[strlen(string) - 1] = 0;
+	}
 }
 
 /*
@@ -291,8 +328,44 @@ LoadScriptFile
 */
 script_s *LoadScriptFile(const char *filename)
 {
-	UNIMPLEMENTED(__FUNCTION__);
-	return NULL;
+	int fp;
+	char pathname[64];
+
+	Com_sprintf(pathname, 64, "%s", filename);
+
+	int v1 = FS_FOpenFileRead(pathname, &fp);
+
+	if (!fp)
+	{
+		return 0;
+	}
+
+	script_s *ClearedMemory = GetClearedMemory(v1 + 1201);
+
+	strcpy(ClearedMemory->filename, filename);
+
+	ClearedMemory->buffer = ClearedMemory[1].filename;
+	ClearedMemory[1].filename[v1] = 0;
+	char *buffer = ClearedMemory->buffer;
+	ClearedMemory->script_p = buffer;
+	ClearedMemory->lastscript_p = buffer;
+	ClearedMemory->length = v1;
+	ClearedMemory->end_p = &buffer[v1];
+	ClearedMemory->tokenavailable = 0;
+	ClearedMemory->line = 1;
+	ClearedMemory->lastline = 1;
+
+	PS_CreatePunctuationTable(ClearedMemory, default_punctuations);
+
+	char *v5 = ClearedMemory->buffer;
+	ClearedMemory->punctuations = default_punctuations;
+
+	FS_Read(v5, v1, fp);
+	FS_FCloseFile(fp);
+
+	ClearedMemory->length = Com_Compress(ClearedMemory->buffer);
+
+	return ClearedMemory;
 }
 
 /*
@@ -302,8 +375,29 @@ LoadScriptMemory
 */
 script_s *LoadScriptMemory(const char *ptr, int length, const char *name)
 {
-	UNIMPLEMENTED(__FUNCTION__);
-	return NULL;
+	script_s *ClearedMemory = GetClearedMemory(length + 1201);
+
+	strcpy(ClearedMemory->filename, name);
+
+	ClearedMemory->buffer = ClearedMemory[1].filename;
+	ClearedMemory[1].filename[length] = 0;
+	char *buffer = ClearedMemory->buffer;
+	ClearedMemory->script_p = buffer;
+	ClearedMemory->lastscript_p = buffer;
+	ClearedMemory->length = length;
+	ClearedMemory->end_p = &buffer[length];
+	ClearedMemory->tokenavailable = 0;
+	ClearedMemory->line = 1;
+	ClearedMemory->lastline = 1;
+
+	PS_CreatePunctuationTable(ClearedMemory, default_punctuations);
+
+	unsigned __int8 *v6 = ClearedMemory->buffer;
+	ClearedMemory->punctuations = default_punctuations;
+
+	memcpy(v6, ptr, length);
+
+	return ClearedMemory;
 }
 
 /*
@@ -313,6 +407,11 @@ FreeScript
 */
 void FreeScript(script_s *script)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (script->punctuationtable)
+	{
+		FreeMemory(script->punctuationtable);
+	}
+
+	FreeMemory(script);
 }
 
