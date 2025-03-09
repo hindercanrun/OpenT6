bcdc3fca4d00dfa5a73991221b03c69cc18df86c
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-03-09 17:53:39 +0800

    complete some of keyvaluepairs.cpp

--- code/src/qcommon/keyvaluepairs.cpp
+++ code/src/qcommon/keyvaluepairs.cpp
@@ -18,7 +18,24 @@ KeyValuePairs_Add
 */
 void KeyValuePairs_Add(KeyValuePairs *kvp, int zoneIndex)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	assertMsg((unsigned)(zoneIndex) < (unsigned)((sizeof( s_keyValuePairs ) / (sizeof( s_keyValuePairs[0] ) * (sizeof( s_keyValuePairs ) != 4
+		|| sizeof( s_keyValuePairs[0] ) <= 4)))),
+		"zoneIndex doesn't index ARRAY_COUNT( s_keyValuePairs )\n\t%i not in [0, %i)",
+		zoneIndex,
+		65);
+
+	if (s_keyValuePairs[zoneIndex])
+	{
+		assert(s_keyValuePairs[zoneIndex] == 0);
+
+		s_keyValuePairs[zoneIndex] = kvp;
+		s_lastLoadedZoneIndex = zoneIndex;
+	}
+	else
+	{
+		s_keyValuePairs[zoneIndex] = kvp;
+		s_lastLoadedZoneIndex = zoneIndex;
+	}
 }
 
 /*
@@ -28,6 +45,20 @@ KeyValuePairs_Remove
 */
 void KeyValuePairs_Remove(KeyValuePairs *kvp)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	int v1 = 0;
+	while (s_keyValuePairs[v1] != kvp)
+	{
+		if (++v1 >= 0x41)
+		{
+			assert("KeyValuePair already removed, or was never added");
+			return;
+		}
+	}
+
+	if (v1 == s_lastLoadedZoneIndex)
+	{
+		s_lastLoadedZoneIndex = -1;
+	}
+	s_keyValuePairs[v1] = 0;
 }
 
