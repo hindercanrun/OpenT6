53ba4d11286366725d1b6471f204b2409a784a5e
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-03-25 20:04:21 +0800

    complete sd_source.cpp

--- code/src/sound/sd_source.cpp
+++ code/src/sound/sd_source.cpp
@@ -5,8 +5,34 @@
 SD_SourceInitStream
 ==============
 */
-void SD_SourceInitStream(sd_source *source, const char *name, const SndAssetBankEntry *entry, int fid, const char *primeData, unsigned int primeSize)
+void SD_SourceInitStream(
+    sd_source *source,
+    const char *name,
+    const SndAssetBankEntry *entry,
+    int fid,
+    const char *primeData,
+    unsigned int primeSize)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+    assertMsg(
+        "entry->channelCount == 1 || entry->channelCount == 2",
+        "(entry->channelCount) = %i",
+        entry->channelCount
+    );
+
+    source->stream = SD_StreamAllocate(name, entry, fid, primeData, primeSize);
+
+    if (source->stream)
+    {
+        source->primed = 0;
+        source->error = 0;
+        source->eos = 0;
+        source->looping = (entry->looping != 0);
+    }
+    else
+    {
+        source->error = 1;
+        source->primed = 1;
+        source->eos = 1;
+    }
 }
 
