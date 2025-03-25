d5556b7b8fafa49edf0d1a27bc81cb4b63c6bb1f
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-03-25 20:11:36 +0800

    complete snd_log.cpp

--- code/src/sound/snd_log.cpp
+++ code/src/sound/snd_log.cpp
@@ -7,6 +7,22 @@ SND_LogMissingAliasId
 */
 void SND_LogMissingAliasId(unsigned int hash)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	assert(hash != NULL);
+
+	Sys_EnterCriticalSection(CRITSECT_SOUND_LOOKUP_CACHE);
+
+	static char reverse[48];
+	SND_DevhostMissingAlias(hash);
+	if (!SND_LogSkip(hash, 0x80u, g_snd.logSkipMissingHash, g_snd.logSkipMissingTimestamp))
+	{
+		SND_LogLookupAlias(hash, reverse);
+
+		if (reverse[0])
+		{
+			BB_Print(INVALID_CONTROLLER_PORT, "sound_string", "string %s", reverse);
+		}
+	}
+
+	Sys_LeaveCriticalSection(CRITSECT_SOUND_LOOKUP_CACHE);
 }
 
