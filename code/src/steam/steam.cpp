a524b1b1bfd65b1eeb2e9ed2cdcaf39f5213f783
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-03-25 20:15:38 +0800

    complete steam.cpp

--- code/src/steam/steam.cpp
+++ code/src/steam/steam.cpp
@@ -7,6 +7,26 @@ Steam_CreateSteamAppIdFile
 */
 void Steam_CreateSteamAppIdFile()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	char buffer[32] = {0};
+
+	FILE *file = fopen("steam_appid.txt", "r");
+	if (!file || !fgets(buffer, sizeof(buffer), file) || strncmp(buffer, "202990", 6) != NULL)
+	{
+		if (file)
+		{
+			fclose(file);
+		}
+
+		file = fopen("steam_appid.txt", "w");
+		if (file)
+		{
+			fprintf(file, "202990");
+			fclose(file);
+		}
+	}
+	else
+	{
+		fclose(file);
+	}
 }
 
