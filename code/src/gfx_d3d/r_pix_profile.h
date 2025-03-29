c595b52878b0a0c5032faf1c214ac4f3ddd15fc0
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-03-29 23:07:24 +0800

    complete r_pix_profile.h

--- code/src/gfx_d3d/r_pix_profile.h
+++ code/src/gfx_d3d/r_pix_profile.h
@@ -7,7 +7,12 @@ PIXBeginNamedEvent
 */
 inline void PIXBeginNamedEvent(int Color, const char *Name, ...)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (Sys_IsRenderThread())
+	{
+		wchar_t w[256] = { 0 };
+		MultiByteToWideChar(0, 0, Name, -1, w, 256);
+		D3DPERF_BeginEvent(Color, w);
+	}
 }
 
 /*
@@ -17,6 +22,11 @@ PIXSetMarker
 */
 inline void PIXSetMarker(int Color, const char *Name, ...)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (Sys_IsRenderThread())
+	{
+		wchar_t w[256] = { 0 };
+		MultiByteToWideChar(0, 0, Name, -1, w, 256);
+		D3DPERF_SetMarker(Color, w);
+	}
 }
 
