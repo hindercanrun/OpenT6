c02b88c4f6c98689c1d7600d35ca193e3d4bdc4f
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-03-29 13:11:25 +0800

    add missing file

---
+++ code/src/gfx_d3d/r_cinematic_webm.cpp
@@ -0,0 +1,204 @@
+#include "types.h"
+
+/*
+==============
+sub_82A3E770
+==============
+*/
+void sub_82A3E770(int a1, int a2, unsigned int a3, unsigned int *a4)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+}
+
+/*
+==============
+sub_82A3E820
+==============
+*/
+void sub_82A3E820()
+{
+	UNIMPLEMENTED(__FUNCTION__);
+}
+
+/*
+==============
+sub_82A3E9C0
+==============
+*/
+int sub_82A3E9C0(const char *a1, char a2, DWORD *a3)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return 0;
+}
+
+/*
+==============
+sub_82A3EA78
+==============
+*/
+int sub_82A3EA78(int a1, int a2, int a3)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return 0;
+}
+
+/*
+==============
+sub_82A3EDE0
+==============
+*/
+int sub_82A3EDE0(int a1, char a2)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return 0;
+}
+
+/*
+==============
+sub_82A3EE88
+==============
+*/
+void sub_82A3EE88()
+{
+	UNIMPLEMENTED(__FUNCTION__);
+}
+
+/*
+==============
+sub_82A3EEA0
+==============
+*/
+int sub_82A3EEA0(int a1, int a2, int a3)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return 0;
+}
+
+/*
+==============
+sub_82A3EF48
+==============
+*/
+int sub_82A3EF48()
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return 0;
+}
+
+/*
+==============
+R_Cinematic_InitWebm
+==============
+*/
+void R_Cinematic_InitWebm()
+{
+	UNIMPLEMENTED(__FUNCTION__);
+}
+
+/*
+==============
+sub_82A3F030
+==============
+*/
+void sub_82A3F030(int a1, int a2)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+}
+
+/*
+==============
+sub_82A3F1F0
+==============
+*/
+void sub_82A3F1F0()
+{
+	UNIMPLEMENTED(__FUNCTION__);
+}
+
+/*
+==============
+sub_82A3F280
+==============
+*/
+void sub_82A3F280()
+{
+	UNIMPLEMENTED(__FUNCTION__);
+}
+
+/*
+==============
+sub_82A3F368
+==============
+*/
+webmPlayback *sub_82A3F368(const char *a1, char a2)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return NULL;
+}
+
+/*
+==============
+sub_82A3F590
+==============
+*/
+webmPlayback *sub_82A3F590(const char *a1)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return NULL;
+}
+
+/*
+==============
+sub_82A3F608
+==============
+*/
+void sub_82A3F608(webmPlayback *a1)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+}
+
+/*
+==============
+gjk_cylinder_t::is_polyhedron
+==============
+*/
+/*
+int gjk_cylinder_t::is_polyhedron(gjk_cylinder_t *this)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return 0;
+}
+*/
+
+/*
+==============
+sub_82A3F680
+==============
+*/
+double sub_82A3F680(webmPlayback *a1)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return NULL;
+}
+
+/*
+==============
+R_Cinematic_GetWebmFunctions
+==============
+*/
+int R_Cinematic_GetWebmFunctions()
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return 0;
+}
+
+/*
+==============
+R_Cinematic_WebmGetTextureBytesUsed
+==============
+*/
+int R_Cinematic_WebmGetTextureBytesUsed(gjk_cylinder_t *a1)
+{
+	UNIMPLEMENTED(__FUNCTION__);
+	return 0;
+}
