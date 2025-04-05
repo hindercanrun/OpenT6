4cc00871ffbd55035724a1f6f19c39f75217e6af
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-04-06 00:52:56 +0800

    Complete R_InitDrawMethod

--- code/src/gfx_d3d/r_draw_method.cpp
+++ code/src/gfx_d3d/r_draw_method.cpp
@@ -17,7 +17,34 @@ R_InitDrawMethod
 */
 void R_InitDrawMethod()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (r_fullbright->current.enabled)
+	{
+		gfxDrawMethod.drawScene = GFX_DRAW_SCENE_FULLBRIGHT;
+		gfxDrawMethod.baseTechType = 514;
+
+		memset(gfxDrawMethod.litTechType[0], 2, sizeof(gfxDrawMethod));
+	}
+	else if (r_debugShader->current.integer >= 1)
+	{
+		gfxDrawMethod.drawScene = GFX_DRAW_SCENE_DEBUGSHADER;
+		gfxDrawMethod.baseTechType = 8738;
+
+		memset(gfxDrawMethod.litTechType[0], 34, sizeof(gfxDrawMethod));
+	}
+	else if (r_debugPerformance->current.integer >= 1)
+	{
+		gfxDrawMethod.drawScene = GFX_DRAW_SCENE_DEBUGSHADER;
+		gfxDrawMethod.baseTechType = 772;
+
+		R_SetDefaultLitTechTypes();
+	}
+	else
+	{
+		gfxDrawMethod.baseTechType = 772;
+		gfxDrawMethod.drawScene = GFX_DRAW_SCENE_STANDARD;
+
+		R_SetDefaultLitTechTypes();
+	}
 }
 
 /*
