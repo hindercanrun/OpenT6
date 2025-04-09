5408210f27abf8a0b3b22baa4ad08b9088025466
hindercanrun <109132519+hindercanrun@users.noreply.github.com>
2025-04-09 13:45:33 +0800

    c

--- code/src/cgame/cg_visionsets.cpp
+++ code/src/cgame/cg_visionsets.cpp
@@ -313,7 +313,110 @@ CG_VisionSetUpdateTweaksFromFile_Film
 */
 void CG_VisionSetUpdateTweaksFromFile_Film()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	visionSetVars_t setVars{};
+	if (!LoadVisionFileForTweaks(&setVars))
+	{
+		Com_PrintWarning(
+			CON_CHANNEL_SYSTEM,
+			"WARNING: Couldn't update film tweak vars from file.  Vision file is likely not in use.\n");
+	}
+
+	Dvar_SetFloat(
+		r_filmLut,
+		setVars.filmLut);
+	Dvar_SetBool(
+		r_filmTweakEnable,
+		setVars.filmEnable);
+	Dvar_SetVec4(
+		vc_RS,
+		setVars.visColorRangeS[0],
+		setVars.visColorRangeS[1],
+		setVars.visColorRangeS[2],
+		setVars.visColorRangeS[3]);
+	Dvar_SetVec4(
+		vc_RE,
+		setVars.visColorRangeE[0],
+		setVars.visColorRangeE[1],
+		setVars.visColorRangeE[2],
+		setVars.visColorRangeE[3]);
+	Dvar_SetVec4(
+		vc_SMR,
+		setVars.visColorShadowMatrixR[0],
+		setVars.visColorShadowMatrixR[1],
+		setVars.visColorShadowMatrixR[2],
+		setVars.visColorShadowMatrixR[3]);
+	Dvar_SetVec4(
+		vc_SMG,
+		setVars.visColorShadowMatrixG[0],
+		setVars.visColorShadowMatrixG[1],
+		setVars.visColorShadowMatrixG[2],
+		setVars.visColorShadowMatrixG[3]);
+	Dvar_SetVec4(
+		vc_SMB,
+		setVars.visColorShadowMatrixB[0],
+		setVars.visColorShadowMatrixB[1],
+		setVars.visColorShadowMatrixB[2],
+		setVars.visColorShadowMatrixB[3]);
+	Dvar_SetVec4(
+		vc_MMR,
+		setVars.visColorMidtoneMatrixR[0],
+		setVars.visColorMidtoneMatrixR[1],
+		setVars.visColorMidtoneMatrixR[2],
+		setVars.visColorMidtoneMatrixR[3]);
+	Dvar_SetVec4(
+		vc_MMG,
+		setVars.visColorMidtoneMatrixG[0],
+		setVars.visColorMidtoneMatrixG[1],
+		setVars.visColorMidtoneMatrixG[2],
+		setVars.visColorMidtoneMatrixG[3]);
+	Dvar_SetVec4(
+		vc_MMB,
+		setVars.visColorMidtoneMatrixB[0],
+		setVars.visColorMidtoneMatrixB[1],
+		setVars.visColorMidtoneMatrixB[2],
+		setVars.visColorMidtoneMatrixB[3]);
+	Dvar_SetVec4(
+		vc_HMR,
+		setVars.visColorHilightMatrixR[0],
+		setVars.visColorHilightMatrixR[1],
+		setVars.visColorHilightMatrixR[2],
+		setVars.visColorHilightMatrixR[3]);
+	Dvar_SetVec4(
+		vc_HMG,
+		setVars.visColorHilightMatrixG[0],
+		setVars.visColorHilightMatrixG[1],
+		setVars.visColorHilightMatrixG[2],
+		setVars.visColorHilightMatrixG[3]);
+	Dvar_SetVec4(
+		vc_HMB,
+		setVars.visColorHilightMatrixB[0],
+		setVars.visColorHilightMatrixB[1],
+		setVars.visColorHilightMatrixB[2],
+		setVars.visColorHilightMatrixB[3]);
+	Dvar_SetVec4(
+		vc_FGM,
+		setVars.visColorFinalGamma[0],
+		setVars.visColorFinalGamma[1],
+		setVars.visColorFinalGamma[2],
+		setVars.visColorFinalGamma[3]);
+	Dvar_SetVec4(
+		vc_FSM,
+		setVars.visColorFinalSaturation[0],
+		setVars.visColorFinalSaturation[1],
+		setVars.visColorFinalSaturation[2],
+		setVars.visColorFinalSaturation[3]);
+	Dvar_SetVec4(
+		vc_FBM,
+		setVars.visColorFinalBlend[0],
+		setVars.visColorFinalBlend[1],
+		setVars.visColorFinalBlend[2],
+		setVars.visColorFinalBlend[3]);
+	Dvar_SetVec4(
+		r_sunFlareTint,
+		setVars.sunFlareTint[0],
+		setVars.sunFlareTint[1],
+		setVars.sunFlareTint[2],
+		setVars.sunFlareTint[3]);
 }
 
 /*
@@ -323,7 +426,23 @@ CG_VisionSetUpdateTweaksFromFile_CharPrimary
 */
 void CG_VisionSetUpdateTweaksFromFile_CharPrimary()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	visionSetVars_t setVars{};
+	if (!LoadVisionFileForTweaks(&setVars))
+	{
+		Com_PrintWarning(
+			CON_CHANNEL_SYSTEM,
+			"WARNING: Couldn't update character primary light tweak vars from file.  Vision file is likely not in use.\n");
+	}
+
+	Dvar_SetBool(
+		r_primaryLightUseTweaks,
+		setVars.charPrimaryUseTweaks);
+	Dvar_SetFloat(
+		r_primaryLightTweakDiffuseStrength,
+		setVars.charPrimaryDiffuseScale);
+	Dvar_SetFloat(
+		r_primaryLightTweakSpecularStrength,
+		setVars.charPrimarySpecularScale);
 }
 
 /*
@@ -333,6 +452,67 @@ CG_VisionSetUpdateTweaksFromFile_Bloom
 */
 void CG_VisionSetUpdateTweaksFromFile_Bloom()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	visionSetVars_t setVars{};
+	if (!LoadVisionFileForTweaks(&setVars))
+	{
+		Com_PrintWarning(
+			CON_CHANNEL_SYSTEM,
+			"WARNING: Couldn't update bloom tweak vars from file.  Vision file is likely not in use.\n");
+	}
+
+	Dvar_SetVec4(
+		vc_LIB,
+		setVars.visBloomLevelsRGBYInB[0],
+		setVars.visBloomLevelsRGBYInB[1],
+		setVars.visBloomLevelsRGBYInB[2],
+		setVars.visBloomLevelsRGBYInB[3]);
+	Dvar_SetVec4(
+		vc_LIG,
+		setVars.visBloomLevelsRGBYInG[0],
+		setVars.visBloomLevelsRGBYInG[1],
+		setVars.visBloomLevelsRGBYInG[2],
+		setVars.visBloomLevelsRGBYInG[3]);
+	Dvar_SetVec4(
+		vc_LIW,
+		setVars.visBloomLevelsRGBYInW[0],
+		setVars.visBloomLevelsRGBYInW[1],
+		setVars.visBloomLevelsRGBYInW[2],
+		setVars.visBloomLevelsRGBYInW[3]);
+	Dvar_SetVec4(
+		vc_LOB,
+		setVars.visBloomLevelsRGBYOutB[0],
+		setVars.visBloomLevelsRGBYOutB[1],
+		setVars.visBloomLevelsRGBYOutB[2],
+		setVars.visBloomLevelsRGBYOutB[3]);
+	Dvar_SetVec4(
+		vc_LOW,
+		setVars.visBloomLevelsRGBYOutW[0],
+		setVars.visBloomLevelsRGBYOutW[1],
+		setVars.visBloomLevelsRGBYOutW[2],
+		setVars.visBloomLevelsRGBYOutW[3]);
+	Dvar_SetVec4(
+		vc_RGBH,
+		setVars.visBloomRGBHi[0],
+		setVars.visBloomRGBHi[1],
+		setVars.visBloomRGBHi[2],
+		setVars.visBloomRGBHi[3]);
+	Dvar_SetVec4(
+		vc_RGBL,
+		setVars.visBloomRGBLo[0],
+		setVars.visBloomRGBLo[1],
+		setVars.visBloomRGBLo[2],
+		setVars.visBloomRGBLo[3]);
+	Dvar_SetVec4(
+		vc_YH,
+		setVars.visBloomYHi[0],
+		setVars.visBloomYHi[1],
+		setVars.visBloomYHi[2],
+		setVars.visBloomYHi[3]);
+	Dvar_SetVec4(
+		vc_YL,
+		setVars.visBloomYLo[0],
+		setVars.visBloomYLo[1],
+		setVars.visBloomYLo[2],
+		setVars.visBloomYLo[3]);
 }
 
--- code/src/cgame_mp/cg_consolecmds_mp.cpp
+++ code/src/cgame_mp/cg_consolecmds_mp.cpp
@@ -1,5 +1,28 @@
 #include "types.h"
 
+static cmd_function_s CG_Viewpos_f_VAR;
+static cmd_function_s CG_NextWeapon_f_VAR;
+static cmd_function_s CG_PrevWeapon_f_VAR;
+static cmd_function_s CG_ShellShock_f_VAR;
+static cmd_function_s CG_ShellShock_Load_f_VAR;
+static cmd_function_s CG_ShellShock_Save_f_VAR;
+static cmd_function_s CG_QuickMessage_f_VAR;
+static cmd_function_s CG_VoiceChat_f_VAR;
+static cmd_function_s CG_TeamVoiceChat_f_VAR;
+static cmd_function_s CG_ToggleScores_f_VAR;
+static cmd_function_s CG_PlayRumble_f_VAR;
+static cmd_function_s CG_FxSetTestPosition_VAR;
+static cmd_function_s CG_FxTest_VAR;
+static cmd_function_s CG_RestartSmokeGrenades_f_VAR;
+static cmd_function_s UpdateFilmTweaks_f_VAR;
+static cmd_function_s UpdateCharPrimaryTweaks_f_VAR;
+static cmd_function_s UpdateBloomTweaks_f_VAR;
+static cmd_function_s CG_PrintFXNameUsage_VAR;
+static cmd_function_s CG_ToggleBandwidthProfiling_f_VAR;
+static cmd_function_s Recorder_TogglePlayback_f_VAR;
+static cmd_function_s Recorder_MemResize_f_VAR;
+static cmd_function_s Recorder_GotoTime_f_VAR;
+
 /*
 ==============
 CG_Viewpos_f
@@ -7,7 +30,17 @@ CG_Viewpos_f
 */
 void CG_Viewpos_f()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (cgArray->nextSnap)
+	{
+		Com_Printf(
+			CON_CHANNEL_DONT_FILTER,
+			"(%.0f %.0f %.0f) : %.0f %.0f\n",
+			cgArray->refdef.vieworg.x,
+			cgArray->refdef.vieworg.y,
+			cgArray->refdef.vieworg.z,
+			cgArray->refdefViewAngles.y,
+			cgArray->refdefViewAngles.x);
+	}
 }
 
 /*
@@ -27,7 +60,13 @@ CG_ScoresUp
 */
 void CG_ScoresUp(LocalClientNum_t localClientNum)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (CG_IsScoreboardDisplayed(localClientNum))
+	{
+		cg_s *LocalClientGlobals = CG_GetLocalClientGlobals(localClientNum);
+		LocalClientGlobals->showScores = false;
+		LocalClientGlobals->scoresTop = -1;
+		LocalClientGlobals->scoreFadeTime = LocalClientGlobals->time;
+	}
 }
 
 /*
@@ -37,7 +76,7 @@ CG_ScoresDown
 */
 void CG_ScoresDown(LocalClientNum_t localClientNum)
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	CG_GetLocalClientGlobals(localClientNum)->showScores = 1;
 }
 
 /*
@@ -67,7 +106,15 @@ CG_ShellShock_Load_f
 */
 void CG_ShellShock_Load_f()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (cgArray->nextSnap)
+	{
+		if (Cmd_Argc() != 2)
+		{
+			Com_Printf(CON_CHANNEL_DONT_FILTER, "USAGE: cg_shellshock_load <name>\n");
+		}
+
+		BG_LoadShellShockDvars(Cmd_Argv(1));
+	}
 }
 
 /*
@@ -77,7 +124,15 @@ CG_ShellShock_Save_f
 */
 void CG_ShellShock_Save_f()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (cgArray->nextSnap)
+	{
+		if (Cmd_Argc() != 2)
+		{
+			Com_Printf(CON_CHANNEL_DONT_FILTER, "USAGE: cg_shellshock_save <name>\n");
+		}
+
+		BG_SaveShellShockDvars(Cmd_Argv(1));
+	}
 }
 
 /*
@@ -117,7 +172,15 @@ CG_PlayRumble_f
 */
 void CG_PlayRumble_f()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	if (cgArray->nextSnap)
+	{
+		if (Cmd_Argc() != 2)
+		{
+			Com_Printf(CON_CHANNEL_DONT_FILTER, "USAGE: playrumble <rumblename>\n");
+		}
+
+		CG_PlayRumbleOnClient(LOCAL_CLIENT_FIRST, Cmd_Argv(1));
+	}
 }
 
 /*
@@ -127,7 +190,7 @@ CG_RestartSmokeGrenades_f
 */
 void CG_RestartSmokeGrenades_f()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	CG_RestartSmokeGrenades(LOCAL_CLIENT_FIRST);
 }
 
 /*
@@ -135,9 +198,9 @@ void CG_RestartSmokeGrenades_f()
 UpdateFilmTweaks_f
 ==============
 */
-void __cdecl UpdateFilmTweaks_f()
+void UpdateFilmTweaks_f()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	CG_VisionSetUpdateTweaksFromFile_Film();
 }
 
 /*
@@ -145,9 +208,9 @@ void __cdecl UpdateFilmTweaks_f()
 UpdateCharPrimaryTweaks_f
 ==============
 */
-void __cdecl UpdateCharPrimaryTweaks_f()
+void UpdateCharPrimaryTweaks_f()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	CG_VisionSetUpdateTweaksFromFile_CharPrimary();
 }
 
 /*
@@ -155,9 +218,9 @@ void __cdecl UpdateCharPrimaryTweaks_f()
 UpdateBloomTweaks_f
 ==============
 */
-void __cdecl UpdateBloomTweaks_f()
+void UpdateBloomTweaks_f()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	CG_VisionSetUpdateTweaksFromFile_Bloom();
 }
 
 /*
@@ -187,7 +250,28 @@ CG_InitConsoleCommandsProject
 */
 void CG_InitConsoleCommandsProject()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	Cmd_AddCommandInternal("viewpos", CG_Viewpos_f, &CG_Viewpos_f_VAR);
+	Cmd_AddCommandInternal("weapnext", CG_NextWeapon_f, &CG_NextWeapon_f_VAR);
+	Cmd_AddCommandInternal("weapprev", CG_PrevWeapon_f, &CG_PrevWeapon_f_VAR);
+	Cmd_AddCommandInternal("cg_shellshock", CG_ShellShock_f, &CG_ShellShock_f_VAR);
+	Cmd_AddCommandInternal("cg_shellshock_load", CG_ShellShock_Load_f, &CG_ShellShock_Load_f_VAR);
+	Cmd_AddCommandInternal("cg_shellshock_save", CG_ShellShock_Save_f, &CG_ShellShock_Save_f_VAR);
+	Cmd_AddCommandInternal("mp_QuickMessage", CG_QuickMessage_f, &CG_QuickMessage_f_VAR);
+	Cmd_AddCommandInternal("VoiceChat", CG_VoiceChat_f, &CG_VoiceChat_f_VAR);
+	Cmd_AddCommandInternal("VoiceTeamChat", CG_TeamVoiceChat_f, &CG_TeamVoiceChat_f_VAR);
+	Cmd_AddCommandInternal("cg_toggleScores", CG_ToggleScores_f, &CG_ToggleScores_f_VAR);
+	Cmd_AddCommandInternal("playrumble", CG_PlayRumble_f, &CG_PlayRumble_f_VAR);
+	Cmd_AddCommandInternal("fxSetTestPosition", CG_FxSetTestPosition, &CG_FxSetTestPosition_VAR);
+	Cmd_AddCommandInternal("fxTest", CG_FxTest, &CG_FxTest_VAR);
+	Cmd_AddCommandInternal("restartsmokegrenades", CG_RestartSmokeGrenades_f, &CG_RestartSmokeGrenades_f_VAR);
+	Cmd_AddCommandInternal("updateFilmTweaks", UpdateFilmTweaks_f, &UpdateFilmTweaks_f_VAR);
+	Cmd_AddCommandInternal("updateCharPrimaryTweaks", UpdateCharPrimaryTweaks_f, &UpdateCharPrimaryTweaks_f_VAR);
+	Cmd_AddCommandInternal("updateBloomTweaks", UpdateBloomTweaks_f, &UpdateBloomTweaks_f_VAR);
+	Cmd_AddCommandInternal("printfxnameusage", CG_PrintFXNameUsage, &CG_PrintFXNameUsage_VAR);
+	Cmd_AddCommandInternal("togglebandwidthprofile", nullptr, &CG_ToggleBandwidthProfiling_f_VAR);
+	Cmd_AddCommandInternal("recorder_togglePlayback", nullptr, &Recorder_TogglePlayback_f_VAR);
+	Cmd_AddCommandInternal("recorder_memResize", nullptr, &Recorder_MemResize_f_VAR);
+	Cmd_AddCommandInternal("recorder_gotoTime", nullptr, &Recorder_GotoTime_f_VAR);
 }
 
 /*
@@ -197,6 +281,21 @@ CG_ShutdownConsoleCommandsProject
 */
 void CG_ShutdownConsoleCommandsProject()
 {
-	UNIMPLEMENTED(__FUNCTION__);
+	Cmd_RemoveCommand("viewpos");
+	Cmd_RemoveCommand("weapnext");
+	Cmd_RemoveCommand("weapprev");
+	Cmd_RemoveCommand("cg_shellshock");
+	Cmd_RemoveCommand("cg_shellshock_load");
+	Cmd_RemoveCommand("cg_shellshock_save");
+	Cmd_RemoveCommand("mp_QuickMessage");
+	Cmd_RemoveCommand("VoiceChat");
+	Cmd_RemoveCommand("VoiceTeamChat");
+	Cmd_RemoveCommand("playrumble");
+	Cmd_RemoveCommand("fxSetTestPosition");
+	Cmd_RemoveCommand("fxTest");
+	Cmd_RemoveCommand("restartsmokegrenades");
+	Cmd_RemoveCommand("updateGlowTweaks");
+	Cmd_RemoveCommand("updateFilmTweaks");
+	Cmd_RemoveCommand("updateBloomTweaks");
 }
 
